a754b1fcc241ebfecada12816d794df8652b0d6d
Emanuel Celayes <ecelayes@gmail.com>
2026-02-05 19:44:46 -0300

    feat: maxwith for desktop

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,8 @@
     });
 </script>
 
-<div class="h-dvh w-full bg-gray-50 text-gray-900 font-sans antialiased overflow-hidden">
-    <slot />
+<div class="h-dvh w-full flex justify-center bg-gray-100 dark:bg-gray-900 font-sans antialiased overflow-hidden">
+    <div class="w-full max-w-md h-full bg-gray-50 dark:bg-gray-950 text-gray-900 shadow-2xl overflow-hidden relative flex flex-col">
+        <slot />
+    </div>
 </div>
